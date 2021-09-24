26f8b405dcf9dfd80ab861a6cbf58142911e6226
jdias <jdias@direct.one>
2021-09-24 13:56:00 -0300

    update

---
+++ menuLateral.php
@@ -0,0 +1,163 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" media="screen" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+      <link
+      href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"
+      rel="stylesheet"
+      id="bootstrap-css"
+    />
+
+
+</head>
+
+<body>
+    <nav class="main-menu">
+        <div class="LogoMenu">
+            <img class="" src="assets/images/logo_unimed.png" style="width: 0%; margin-left: 6%" alt="Unimed" />
+        </div>
+        <ul>
+            <li>
+                <div class="divBusca">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                    <span class="nav-text">
+                        <div class="grpBusca" style="display: flex;">
+                            <input type="text" name="txtbusca" id="txtbusca" placeholder="Pequisar...">
+                            <button type="button" name="btnBuscar" id="btnBuscar"><i class="fas fa-search" style="color:white"></i></button>
+                        </div>
+                    </span>
+                </div>
+            </li>
+            <li class="has-subnav">
+                <a href="http://justinfarrow.com">
+                    <i class="fa fa-home"></i>
+                    <span class="nav-text">
+                        Inicio
+                    </span>
+                </a>
+
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                    <i class="fa fa-list "></i>
+                    <span class="nav-text">
+                        Tabela
+                    </span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="">Usuário</a></li>
+                    <li><a href="">Perfil do Usuário</a></li>
+                    <li><a href="">Setor</a></li>
+                    <li><a href="">Motivo de Compras</a></li>
+                    <li><a href="">Classificação de Produtos</a></li>
+                    <li><a href="">Produto</a></li>
+                    <li><a href="">Fornecedor</a></li>
+                    <li><a href="">Motivo de Reembolso</a></li>
+                    <li><a href="">Configuração de Autorização</a></li>
+                    <li><a href="">Tipo de solicitação</a></li>
+                </ul>
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    <span class="nav-text">
+                        Cobrança
+                    </span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="">Resumo</a></li>
+                    <li><a href="">Solicitação</a></li>
+                    <li><a href="">Carta de Comunicação</a></li>
+                    <li><a href="">Segunda Via de Boleto</a></li>
+                    <li><a href="">XLS de Solicitaçao</a></li>
+                    <li><a href="">Clientes Suspensos</a></li>
+                </ul>
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                <i class="fa fa-money" aria-hidden="true"></i>
+                    <span class="nav-text">
+                        Financeiro
+                    </span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="">Boleto</a></li>
+                    <li><a href="">Nota Fiscal</a></li>
+                    <li><a href="">Contas a Receber</a></li>
+                    <li><a href="">Contas a Pagar</a></li>
+                </ul>
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    <span class="nav-text">
+                        Compras
+                    </span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="">Resumo</a></li>
+                    <li><a href="">Solicitação de Compras</a></li>
+                    <li><a href="">Cotação de Compras</a></li>
+                </ul>
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                <i class="fa fa-check-circle" aria-hidden="true"></i>
+                    <span class="nav-text">
+                        Autorização
+                    </span>
+                </a>
+                <ul class="sub-menu">
+                    <li><a href="">Resumo</a></li>
+                    <li><a href="">Solicitação de Compras</a></li>
+                    <li><a href="">Cotação de Compras</a></li>
+                </ul>
+            </li>
+            <li class="has-subnav">
+                <a href="#">
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                    <span class="nav-text">
+                        Controles
+                    </span>
+                </a>
+                <ul class="sub-menu">
+                    <li class="">
+                        <a href="">Protocolo SAC</a>
+                        <ul>
+                            <li  class="sub-sub-menu"><a href="">Resumo SAC</a></li>
+                            <li  class="sub-sub-menu"><a href="">Protocolos</a></li>
+                            <li  class="sub-sub-menu"><a href="">Geração do XLS</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Protocolo Jurídico</a>
+                        <ul> 
+                            <li  class="sub-sub-menu"><a href="">Protocolo NIP</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Protocolo de Ouvidoria</a>
+                        <ul>
+                            <li  class="sub-sub-menu"><a href="">Resumo de ouvidoria</a></li>
+                            <li  class="sub-sub-menu"><a href="">Protocolos</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+        <ul class="logout">
+            <li>
+                <a href="#">
+                    <i class="fa fa-power-off "></i>
+                    <span class="nav-text">
+                        Logout
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</body>
+
+</html>
